a2187755044cd98e9385c44904f86c31ee17220c
sanjanachunduri <sc65@illinois.edu>
2019-11-25 14:15:24 -0600

    setting up

---
+++ app/src/main/java/User.java
@@ -0,0 +1,14 @@
+/** Updates and holds each users information */
+public class User {
+    private String name;
+    private int age;
+    private String grade;
+    private String major;
+    private String gender;
+    public User(String n, int a, String g, String gend) {
+        name = n;
+        age = a;
+        grade = g;
+        gender = gend;
+    }
+}
