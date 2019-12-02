628f42a22acbb9a7e46bfeaa7589f1e5868c3ec9
sanjanachunduri <sc65@illinois.edu>
2019-12-02 03:19:47 -0600

    updated

--- app/src/main/java/LoginActivity.java
+++ app/src/main/java/LoginActivity.java
@@ -1,2 +1,21 @@
-public class LoginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/** Login Screen Activity and creating new user account */
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(final Bundle savedState) {
+        super.onCreate(savedState);
+        setContentView(R.layout.loginscreen);
+        Button createAccount = findViewById(R.id.createAccount);
+
+    }
+
+
 }
