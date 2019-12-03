bb31c6d799727aa0c4ac0d66d6ff63a24d497a59
sanjanachunduri <sc65@illinois.edu>
2019-12-03 10:49:56 -0600

    ui finish

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("hi", "ok");
         setContentView(R.layout.login_screen);
 
         username = findViewById(R.id.userName);
