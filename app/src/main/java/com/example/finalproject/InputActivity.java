4bb892c3734f85ef15562474de1505e549da521d
DESKTOP-9Q587T5\shiva <vishnu@execideas.com>
2019-12-06 01:43:01 -0600

    input activity one button

--- app/src/main/java/com/example/finalproject/InputActivity.java
+++ app/src/main/java/com/example/finalproject/InputActivity.java
@@ -1,14 +1,36 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InputActivity extends AppCompatActivity {
+    private ImageButton mood;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_page);
 
+        mood = findViewById(R.id.btnHappy);
+
+        mood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fillInGrid();
+            }
+        });
+
+    }
+
+    private void fillInGrid() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        setContentView(R.layout.calendar);
     }
 }
