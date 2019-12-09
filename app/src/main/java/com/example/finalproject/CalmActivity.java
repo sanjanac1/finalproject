ede2cc78b29476781177aea7d3c13ed5af98cb93
sanjanachunduri <sc65@illinois.edu>
2019-12-09 06:12:54 -0600

    ddd

--- app/src/main/java/com/example/finalproject/CalmActivity.java
+++ app/src/main/java/com/example/finalproject/CalmActivity.java
@@ -1,4 +1,52 @@
 package com.example.finalproject;
 
-public class CalmActivity {
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalmActivity extends AppCompatActivity {
+    private Button pauseCalm;
+    private Button backCalm;
+    private MediaPlayer song;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calm);
+
+        pauseCalm = findViewById(R.id.psCalm);
+        backCalm = findViewById(R.id.backCalm);
+
+        song = MediaPlayer.create(CalmActivity.this, R.raw.calmsong);
+        song.start();
+        pauseCalm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (song.isPlaying()) {
+                    song.pause();
+                } else {
+                    song.start();
+                }
+
+            }
+        });
+        backCalm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                song.stop();
+                Intent intent = new Intent(CalmActivity.this, InputActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+
 }
+
--- app/src/main/java/com/example/finalproject/EnergeticActivity.java
+++ app/src/main/java/com/example/finalproject/EnergeticActivity.java
@@ -1,4 +1,52 @@
 package com.example.finalproject;
 
-public class EnergeticActivity {
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnergeticActivity extends AppCompatActivity {
+    private Button pauseEnergy;
+    private Button backEnergy;
+    private MediaPlayer song;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.energetic);
+
+        pauseEnergy = findViewById(R.id.psenergetic);
+        backEnergy = findViewById(R.id.backEnergetic);
+
+        song = MediaPlayer.create(EnergeticActivity.this, R.raw.energysong);
+        song.start();
+        pauseEnergy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (song.isPlaying()) {
+                    song.pause();
+                } else {
+                    song.start();
+                }
+
+            }
+        });
+        backEnergy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                song.stop();
+                Intent intent = new Intent(EnergeticActivity.this, InputActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+
 }
+
