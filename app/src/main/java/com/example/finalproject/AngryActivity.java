dcdc06b10598a5f0017d90c06e494ce5d4dbc0bc
sanjanachunduri <sc65@illinois.edu>
2019-12-09 06:08:56 -0600

    Merge branch 'master' of github.com:/sanjanac1/finalproject
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/finalproject/AngryActivity.java
+++ app/src/main/java/com/example/finalproject/AngryActivity.java
@@ -1,4 +1,52 @@
 package com.example.finalproject;
 
-public class AngryActivity {
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AngryActivity extends AppCompatActivity {
+    private Button pauseAngry;
+    private Button backAngry;
+    private MediaPlayer song;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.angry);
+
+        pauseAngry = findViewById(R.id.psangry);
+        backAngry = findViewById(R.id.backAngry);
+
+        song = MediaPlayer.create(AngryActivity.this, R.raw.angrysong);
+        song.start();
+        pauseAngry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (song.isPlaying()) {
+                    song.pause();
+                } else {
+                    song.start();
+                }
+
+            }
+        });
+        backAngry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                song.stop();
+                Intent intent = new Intent(AngryActivity.this, InputActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+
 }
+
--- app/src/main/java/com/example/finalproject/HappyActivity.java
+++ app/src/main/java/com/example/finalproject/HappyActivity.java
@@ -1,4 +1,6 @@
 package com.example.finalproject;
 
-public class HappyActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HappyActivity extends AppCompatActivity {
 }
