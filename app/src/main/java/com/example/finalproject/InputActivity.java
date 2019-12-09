f89abde2fb2001c2328c7ffce80a7cde31664662
sanjanachunduri <sc65@illinois.edu>
2019-12-09 17:47:29 -0600

    yesssss

--- app/src/main/java/com/example/finalproject/InputActivity.java
+++ app/src/main/java/com/example/finalproject/InputActivity.java
@@ -2,11 +2,8 @@ package com.example.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.media.MediaPlayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
