d40622041ab79e0adf5dad1b635127e1d7d8015b
sanjanachunduri <sc65@illinois.edu>
2019-12-09 03:09:39 -0600

    yes

--- app/src/main/java/com/example/finalproject/InputActivity.java
+++ app/src/main/java/com/example/finalproject/InputActivity.java
@@ -10,29 +10,82 @@ import android.graphics.Rect;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InputActivity extends AppCompatActivity {
-    private ImageButton mood;
+    private ImageButton angryButton;
+    private ImageButton energyButton;
+    private ImageButton happyButton;
+    private ImageButton lonelyButton;
+    private ImageButton sleepyButton;
+    private ImageButton productiveButton;
+    private ImageButton sadButton;
+    private ImageButton calmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_page);
 
-        mood = findViewById(R.id.btnHappy);
+        happyButton = findViewById(R.id.btnHappy);
+        angryButton = findViewById(R.id.btnAngry);
+        energyButton = findViewById(R.id.btnEnergetic);
+        lonelyButton = findViewById(R.id.btnLonely);
+        sleepyButton = findViewById(R.id.btnSleepy);
+        productiveButton = findViewById(R.id.btnProductive);
+        sadButton = findViewById(R.id.btnSad);
+        calmButton = findViewById(R.id.btnCalm);
 
-        mood.setOnClickListener(new View.OnClickListener() {
+
+        happyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fillInGrid();
+                setContentView(R.layout.calendar);
             }
         });
 
-    }
+        angryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calendar);
+            }
+        });
+        energyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calendar);
+            }
+        });
+        lonelyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calendar);
+            }
+        });
+        sleepyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calendar);
+            }
+        });
+        productiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calendar);
+            }
+        });
+        sadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calendar);
+            }
+        });
+        calmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.calendar);
+            }
+        });
 
-    private void fillInGrid() {
 
-        //Intent intent = new Intent(this, MainActivity.class);
-
-        //startActivity(intent);
-        setContentView(R.layout.calendar);
     }
 }
+
+
